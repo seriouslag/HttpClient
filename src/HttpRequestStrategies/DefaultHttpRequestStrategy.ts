f4920526fab86cee90d4f944c6417f1b77f9ff58
seriouslag <Landon.Gavin@gmail.com>
2021-12-13 20:04:03 -0500

    DOC: expand comments

--- src/HttpRequestStrategies/DefaultHttpRequestStrategy.ts
+++ src/HttpRequestStrategies/DefaultHttpRequestStrategy.ts
@@ -3,7 +3,7 @@ import { HttpResponse, getIsSuccessfulHttpStatus, HttpRequestStrategy } from '..
 
 /** The default HTTP request strat. No logic. */
 export class DefaultHttpRequestStrategy implements HttpRequestStrategy {
-  /** Passthrough request to axios */
+  /** Passthrough request to axios and check response is successful */
   public async request<T = unknown> (client: AxiosInstance, axiosConfig: AxiosRequestConfig) {
     const response = await client.request<T>(axiosConfig);
     this.checkResponseStatus<T>(response);
