d296b74bdba7190c38df79c315ff16a60a3610ae
seriouslag <Landon.Gavin@gmail.com>
2021-12-14 23:59:15 -0500

    Strategy - Change MaxRetry to reuse exisiting logic

--- src/HttpRequestStrategies/MaxRetryHttpRequestStrategy.ts
+++ src/HttpRequestStrategies/MaxRetryHttpRequestStrategy.ts
@@ -1,35 +1,25 @@
 import { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
-import { HttpRequestStrategy } from './HttpRequestStrategy';
-import { getIsSuccessfulHttpStatus } from '../utilities/getIsSuccessfulHttpStatus';
+import { ExponentialBackoffRequestStrategy } from './ExponentialBackoffRequestStrategy';
 
 /** Retrys HTTP requests immediatly on non successful HTTP request until the max retry count.
  *  Stops retrying when a TOO MANY REQUESTS STATUS is recieved (status code: 429)
  */
-export class MaxRetryHttpRequestStrategy implements HttpRequestStrategy {
-
-  /** TOO MANY REQUESTS STATUS CODE */
-  private TOO_MANY_REQUESTS_STATUS = 429;
+export class MaxRetryHttpRequestStrategy extends ExponentialBackoffRequestStrategy {
 
   /**
    * @param maxRetryCount - The maximum number of retries to attempt, default is 5, set to 0 for indefinite retries
    */
-  constructor (private maxRetryCount: number = 5) { }
+  constructor (maxRetryCount: number = 5) {
+    super({
+      delayFirstRequest: false,
+      maxRetryCount,
+      baseDelay:         0,
+      factor:            1,
+      maxDelay:          0,
+    });
+  }
 
-  public async request<T = unknown> (client: AxiosInstance, axiosConfig: AxiosRequestConfig): Promise<AxiosResponse<T, any>> {
-    let response: AxiosResponse<T, any>;
-    let retryCount = 0;
-    let isSuccessfulHttpStatus = false;
-    let isTooManyRequests = false;
-    let isAtRetryLimit = false;
-
-    const increment = this.maxRetryCount <= 0 ? 0 : 1;
-    do {
-      retryCount += increment;
-      response = await client.request<T>(axiosConfig);
-      isSuccessfulHttpStatus = getIsSuccessfulHttpStatus(response.status);
-      isTooManyRequests = response.status === this.TOO_MANY_REQUESTS_STATUS;
-      isAtRetryLimit = this.maxRetryCount === 0 ? false : retryCount >= this.maxRetryCount;
-    } while (!isSuccessfulHttpStatus && !isTooManyRequests && !isAtRetryLimit);
-    return response;
+  public override async request<T = unknown> (client: AxiosInstance, axiosConfig: AxiosRequestConfig): Promise<AxiosResponse<T, any>> {
+    return await super.request(client, axiosConfig);
   }
 }
